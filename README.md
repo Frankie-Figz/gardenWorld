ef2fdb5a4515c51fe200b18d098eb51d4172587f
FrankieFigz <fjvannini@gmail.com>
2019-10-24 22:12:58 -0400

    Cleaned up console logs and added better comments. Added validation for input

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# gardenWorld
-This is an online store where you can buy garden related products
+# Garden World
+ Garden World is an online market place where our customers can purchase garden related products. On the backend of things, we have a team of managers and supervisors who through a handy set of high functions can monitor the business.
+
+ The permissible functions available through the CLI are split up into the following modules :
+
+<ul>
+  <li> gardenWorldClient</li>
+  <li> gardenWorldManager </li>
+  <li> gardenWorldSupervisor </li>
+</ul>
+
+# Functionality
+Documentation of the functionality can be found here :
+<a href=" "> PDF</a>
+
+## gardenWorldClient
+  The Garden World Client module displays the product catalogue alongside the current stock per product. After displaying the catalogue it prompts the user to enter a product and the qty of the product that they wish to purchase.  <br>
+  
+  Their are a validation to assure that valid numbers are entered. Examples of invalid inputs that are registered would be 
+
+## gardenWorldManager
+  The Garden World Manager module is used to monitor the inventory levels and maintain the product catalogue. The functions available are :
+
+<ul>
+  <li> View Low Inventory</li>
+  <li> Add Product to Catalogue </li>
+  <li> Add Product Stock to Inventory </li>
+  <li> View Product Catalogue </li>
+</ul>
+ 
+## gardenWorldSupervisor
+  The functions in the supervisor module are below. The main functionality reflects the department costs based on the product sales (qty * price is added to product cost every time a sale is made).
+
+  <ul>
+  <li> Adding New Department</li>
+  <li> Checking Department Costs </li>
+
+ </ul>
+ 
+## Do-what-it-says
+ The "Do-what-it-says" function executes instruction that are written in the "Random.txt" file. 
+ For example, if you were to have "movie-this,jaws" in the text file then the movie-this function would be executed with the string of "jaws".
+
+# Technology Stack
+The main language of this project is Javascript using mySQL as the database.
+
+The Node libraries used for this project are the following.
+<ul>
+  <li> mysql </li>
+    A library used to interface javascript and mysql.
+  <li> dotenv </li>
+    A library for hiding sensitive data from the code.
+  <li> Inquirer </li>
+    A library for prompting the user for input.
+</ul>
--- gardenWorldClient.js
+++ gardenWorldClient.js
@@ -24,6 +24,15 @@ var connection = mysql.createConnection({
     database: keys.mysqlEnv.database
 });
 
+// initializing mysql connection
+connection.connect(function(err){
+    if(err) throw(err);
+    console.log("===================== WELCOME TO GARDEN WORLD ! =====================");
+    // run the start function which is where we will prompt the customer for the product they wish to purchase
+    viewProducts();
+});
+
+// Function used to iterate over product ids which will later be displayed
 function createChoicesArray(a){
     let arrayID = [];
     for(var i in a)
@@ -32,37 +41,35 @@ function createChoicesArray(a){
     return arrayID;
 };
 
+// Function used to extract the current qty and price attributes of a given product.
 function queryProductStock(id){
     let sqlQueryCurrentQty = "SELECT stock_quantity, price FROM product WHERE product_id = " + id;
 
     connection.query(sqlQueryCurrentQty, function(err, response) {
         if(err){
             console.log("Error displaying products: " + err);
-            // connection.end();
         } else {
-            console.log("I am here trying to stock it !");
             currentQtyProduct = response[0].stock_quantity;
             currentPriceProduct = response[0].price;
             console.log("------------------------------");
-            // connection.end();
         }
     });
 }
 
+// 
 function consumeProduct(id,qty){
-    // Obtains product stock
-    console.log("Inside the consume statement : " + currentQtyProduct);
-
-    // Consumes the product existences
+    // The variable which will be used to consumee the product stock
     currentQtyProduct = parseInt(currentQtyProduct) - parseInt(qty);
 
-    console.log("Current Qty Product : " + currentQtyProduct);
+    console.log("Current Qty Product : " + currentQtyProduct.toFixed(2));
     console.log("Product ID : " + id);
+    console.log("Product Price is  : " + currentPriceProduct.toFixed(2));
+
 
     // Forms sql query to update product
     let sqlQueryUpdateQty = "UPDATE product SET stock_quantity = ?, product_sales = product_sales + ? WHERE product_id = ?";
 
-    // Consumes the query and hopefully updates the product qty
+    // Consumes the query and updates the product qty to the new inventory level after the customer sale
     connection.query(sqlQueryUpdateQty, [currentQtyProduct,currentPriceProduct*inquirerProducyQty,id] ,function(err,response){
         if(err){
             console.log(" Error with product update: " + err);
@@ -74,6 +81,7 @@ function consumeProduct(id,qty){
 
 };
 
+// This function compares the qty entered by the user to the stock of the product on hand
 function compareProductQty(){
     if(inquirerProducyQty > currentQtyProduct)
         console.log("Not enough product qty on hand ! " + "The requested " + inquirerProducyQty + "units is more than current stock of " + currentQtyProduct + " units !");
@@ -81,7 +89,9 @@ function compareProductQty(){
         consumeProduct(currentProductID,inquirerProducyQty);
 };
 
+// The main function of the client. This ties together all the above functions and is also used to recurse over a confirm prompt.
 function viewProducts() {
+    // Query used to display the product catalogue alongside the quantity stock
     let sqlQuery = "SELECT * FROM product";
     connection.query(sqlQuery, function (err, response) {
       if (err) {
@@ -91,6 +101,7 @@ function viewProducts() {
         console.log("------------------------------");
         console.log(createChoicesArray(response));
 
+        // Iterates over response and logs the table
         for (var i = 0; i < response.length; i++)
           console.log(response[i].product_id + " : " + response[i].product_name + ", " + response[i].price + " , " + response[i].department_id + ", " + response[i].stock_quantity + ", " + response[i].product_sales);
         
@@ -101,27 +112,33 @@ function viewProducts() {
                 type: "list",
                 message: "Which product would you like to purchase ?",
                 name: "itemDB",
-                choices: createChoicesArray(response)},
+                choices: createChoicesArray(response)
+            },
             {
                 type: "input",
                 message: "How much qty of the product would you like to buy ?",
-                name: "productQty"
+                name: "productQty",
+                validate: function(input){
+                    if (isNaN(input) == false) 
+                    return true;
+                else
+                    return false;
+                }
             }]).then(function(inquirerResponse) {
                 inquirerProducyQty = inquirerResponse.productQty;
                 currentProductID = inquirerResponse.itemDB;
-                // This function updates the qty on the hand (set as a global variable) for the item selected
+                // This function updates the qty on the hand (set as a global variable) for the item selected. The timeout is used in order to deal with the asynchronous nature of node
                 queryProductStock(inquirerResponse.itemDB);
 
-                // Once the product stock quantity have been 
+                // Once the product stock quantity have been update then we can compare. The timeout is used in order to deal with the asynchronous nature of node
                 setTimeout(compareProductQty,1000);
 
-                // Re-prompts the user for the products on the inventory list
+                // Re-prompts the user for the products on the inventory list. The timeout is used in order to deal with the asynchronous nature of node
                 setTimeout(viewProducts,1500);
                 });
                 
+        console.log("Thank you for your purchase ! The total of your invoice is : $ "  + (inquirerProducyQty * currentPriceProduct).toFixed(2) );
         console.log("------------------------------");        
       }
     });
   };
-
-viewProducts();
